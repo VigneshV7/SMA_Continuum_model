94a4c675895a663829c62ec55ca81b60a2cd6569
Vignesh Venkatachalam <vignesh.venkatachalam@tuhh.de>
2021-09-10 09:34:00 +0200

    Gif for CC model

--- Codes/Constant_curvature_plots.m
+++ Codes/Constant_curvature_plots.m
@@ -1,12 +1,13 @@
 % Code to generate representative plot for CC model
-clear all
+clear
 clc
 
 %% Calculate transforms along the curvature and plot
 length = 1; % 1m
-temp_points = [];
-figure(1); hold on
+figure; hold on
+
 for alpha = pi/10:pi/10:pi
+    temp_points = [];
     for s = 0:0.002:1
         T_1 = homogeneousTransformMatrix(s, alpha, length);
         px = T_1(1, 4);
@@ -14,10 +15,56 @@ for alpha = pi/10:pi/10:pi
         temp_points = [temp_points; [px, py]]; 
     end
     plot(temp_points(:, 1), temp_points(:, 2), '.');
-    temp_points = [];
 end
+
+% Plot parameters
 grid on
+xlim([-0.1 0.8])
+ylim([-0.1 1.1])
 xlabel("x [m]")
 ylabel("y [m]")
 title("Soft continuum arm with constant curvature. L = 1m, alpha = pi/10:pi/10:pi")
 hold off
+
+% Save plot
+saveas(gcf, '../Images/CCplots.png')
+
+%% Code to generate gif
+length = 1; % 1m
+
+h = figure;
+filename = '../Images/CCmodel.gif';
+axis tight manual
+grid on
+
+for alpha = pi/10:pi/10:pi
+    temp_points = [];
+    for s = 0:0.002:1
+        T_1 = homogeneousTransformMatrix(s, alpha, length);
+        px = T_1(1, 4);
+        py = T_1(2, 4);
+        temp_points = [temp_points; [px, py]]; 
+    end
+    
+    % Plot the current curve
+    plot(temp_points(:, 1), temp_points(:, 2), '.');
+    xlabel("x [m]")
+    ylabel("y [m]")
+    title("Soft continuum arm with constant curvature. L = 1m, alpha = pi/10:pi/10:pi")
+    xlim([-0.1 0.8])
+    ylim([-0.1 1.1])
+    grid on
+    drawnow
+    
+    % Capture the plot as an image
+    frame = getframe(h);
+    im = frame2im(frame);
+    [imind, cm] = rgb2ind(im, 256);
+    
+    % Write to the GIF file
+    if alpha == pi/10
+        imwrite(imind, cm, filename, 'gif', 'Loopcount', inf);
+    else
+        imwrite(imind, cm, filename, 'gif', 'WriteMode', 'append');
+    end
+end
