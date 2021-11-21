afdb23aa540cf16df7102c96e915d9c1d69d57a3
Vignesh Venkatachalam <vignesh.venkatachalam@tuhh.de>
2021-11-21 23:13:13 +0100

    simple simulation code written

--- Codes/FKineSMA.m
+++ Codes/FKineSMA.m
@@ -5,5 +5,17 @@
 % continuum module
 function T = FKineSMA(sma_length, d, l_nf)
 alpha = (l_nf - sma_length)/d;
-T = CCSegment(alpha, l_nf);
+transform_mat = CCSegment(alpha, l_nf);
+
+p_x = transform_mat(1, 4);
+p_z = transform_mat(3, 4);
+rotation_mat = transform_mat(1:3, 1:3);
+eulZYX = rotm2eul(rotation_mat);
+alpha = eulZYX(2);
+
+% Create struct array
+field1 = 'tip_position'; value1 = [p_x p_z];
+field2 = 'alpha'; value2 = alpha;
+field3 = 'transform_mat'; value3 = transform_mat;
+T = struct(field1, value1, field2, value2, field3, value3);
 end
--- Codes/model_plots.m
+++ Codes/model_plots.m
@@ -1,11 +1,11 @@
 clear
 clc
 %% Parameters
-l_nf = 0.1;    % 0.1m
-d = 0.005;
+l_nf = 1;    % 1m
+d = 0.05;
 
 %% Unguided(circular arc) vs guided(piecewise linear segments) sma wires
-beta = deg2rad(90);    % target angle
+beta = deg2rad(180);    % target angle
 l_1 = l_nf - d*beta;
 angle_arr = [];
 lin_sma_arr = [];
@@ -50,7 +50,7 @@ title("Relation between l_1^l and l_1(Circular arc) with regards to num of space
 lgd = legend('sma line', 'sma arc', '1% error', '0.2% error');
 lgd.Location = 'southeast';
 hold off 
-saveas(gcf, '../Images/linear_circular_length_diff.png')
+saveas(gcf, '../Images/linear_circular_length_diff_180deg.png')
 
 % figure(2); hold on
 % plot(num_spacers, error_line, num_spacers, zero_line,...
@@ -73,28 +73,43 @@ ylabel("Angle of section [deg]")
 title("Relation between target angle and actual angle with regards to num of spacers")
 legend('sma line', 'sma arc', '1% error', '0.2% error')
 hold off 
-saveas(gcf, '../Images/linear_circular_angle_diff.png')
+saveas(gcf, '../Images/linear_circular_angle_diff_180deg.png')
 
 %% Workspace plot
+l_nf = 1;
+d = 0.05;
+num_spacers = 100;
 points_arr = [];
-for l_1 = 0.0686:0.0001:0.1314   % Corresponds to +2pi and -2pi
+points_arr_restricted = [];
+l_neg360 = IKineSMA(-2*pi, l_nf, d, num_spacers);
+l_pos360 = IKineSMA(2*pi, l_nf, d, num_spacers);
+l_1_neg360 = l_neg360(1);
+l_1_pos360 = l_pos360(1);
+for l_1 = l_1_pos360:0.001:l_1_neg360   % Corresponds to +2pi and -2pi
     T = FKineSMA(l_1, d, l_nf);
     points_arr = [points_arr; [T(1, 4), T(3, 4)]];
 end
+for l_1 = (l_nf-(0.02*l_nf)):0.001:(l_nf+(0.02*l_nf))   % Corresponds to 2% strain
+    T = FKineSMA(l_1, d, l_nf);
+    points_arr_restricted = [points_arr_restricted; [T(1, 4), T(3, 4)]];
+end
 figure(4); hold on
 plot(points_arr(:, 1), points_arr(:, 2), 'x')
+plot(points_arr_restricted(:, 1), points_arr_restricted(:, 2), '+')
 grid on
 axis equal
 xlabel("x [m]")
 ylabel("z [m]")
 title("Workspace of continuum arm (Constant curvature)")
+lgd = legend('Unrestricted', 'Strain-restricted');
+lgd.Location = 'best';
 hold off
 saveas(gcf, '../Images/workspace.png')
 
 %% Curve fitting to derive equation
 
-l_nf = 0.1;
-d = 0.005;
+l_nf = 1;
+d = 0.05;
 n = 10;
 
 alpha_list = [];
